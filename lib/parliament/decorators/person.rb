fe3d84885378cdb0f830f0d2cccb01de57e5ae06
EBIRIM <ebirima@EBIRIMs-MacBook-Pro.local>
2017-02-06 15:27:32 +0000

    committed code for party decorator pattern

--- lib/parliament/decorators/person.rb
+++ lib/parliament/decorators/person.rb
@@ -1,3 +1,4 @@
+
 module Parliament
   module Decorators
     module Person
---
+++ spec/parliament/decorators/party_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../../spec_helper'
+
+describe Parliament::Decorators::Party do
+  let(:data) { StringIO.new(File.read('spec/fixtures/parties_current.nt')) }
+  let(:objects) { Grom::Reader.new(data).objects }
+  let(:party_node) {objects.first }
+
+  describe "#name" do
+    context 'Grom::Node has all the required objects' do
+
+      it 'confirms that the type for this Grom::Node object is Party' do
+
+        expect(party_node.type).to eq('http://id.ukpds.org/schema/Party')
+      end
+
+      it 'returns the name of the party for the Grom::Mode object' do
+
+        party_node.extend(Parliament::Decorators::Party)
+
+        expect(party_node.name).to eq("Labour")
+
+      end
+    end
+
+
+  end
+end
